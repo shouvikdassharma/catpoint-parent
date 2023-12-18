efa398a5704521aec2172c8b373bf495c51c1561
Shouvik <shouvik.sharma@shell.com>
2023-12-18 11:50:58 +0530

    18 dec

--- starter/catpoint-parent/Security/src/main/java/com/udacity/catpoint/security/data/Sensor.java
+++ starter/catpoint-parent/Security/src/main/java/com/udacity/catpoint/security/data/Sensor.java
@@ -10,6 +10,8 @@ import java.util.UUID;
  * Sensor POJO. Needs to know how to sort itself for display purposes.
  */
 public class Sensor implements Comparable<Sensor> {
+    public Sensor() {
+    }
     private UUID sensorId;
     private String name;
     private Boolean active;
